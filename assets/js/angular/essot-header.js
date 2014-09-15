c839d753b33f945f8b52a078668f420de8fda972
unknown <ankit.verma@MININT-7D2CT12.emea.akqa.local>
2014-09-15 10:49:34 +0530

    Angular JS in header.jsp

--- assets/js/angular/essot-header.js
+++ assets/js/angular/essot-header.js
@@ -1,37 +1,12 @@
 var essotHeaderApp = angular.module('essotHeaderApp', []);
 
-
-
- essotHeaderApp.config(['$httpProvider', function($httpProvider) {
-
-          $httpProvider.defaults.useXDomain = true;
-
-
-
-          /**
-
-           * Just setting useXDomain to true is not enough. AJAX request are also
-
-           * send with the X-Requested-With header, which indicate them as being
-
-           * AJAX. Removing the header is necessary, so the server is not
-
-           * rejecting the incoming request.
-
-           **/
-
-          delete $httpProvider.defaults.headers.common['X-Requested-With'];
-
-      }
-
-  ]);
-
-
-
 essotHeaderApp.controller('essotHeaderController', function($scope, $http, $location) {
 
-
-
- $scope.temp = 'ding';
-
+  $http.get('http://122.160.164.121:8080/essotg/rest/category/menu')
+       .success(function(data) {
+         $scope.loaded    = true;
+         $scope.menuList  = data;
+    }).error(function(err) {
+        console.log(err);
+   });   
 });
