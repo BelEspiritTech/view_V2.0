256b32bfcc8091f87a49f3ac82cb9f8569ea6c2c
unknown <ankit.verma@MININT-7D2CT12.emea.akqa.local>
2014-09-19 10:10:59 +0530

    Added category page.

---
+++ assets/js/angular/essot-category.js
@@ -0,0 +1,7 @@
+var essotCategoryApp = angular.module('essotCategoryApp', []);
+
+essotCategoryApp.controller('essotCategoryController', function($scope, $http, $location) {
+
+ $scope.tempo = 'ding';
+
+});
--- assets/js/common.js
+++ assets/js/common.js
@@ -73,7 +73,41 @@
 
 	               	// jQuery UItoTop
 	                tpj().UItoTop({ easingType: 'easeOutQuart' });
-                
+
+
+                    // Carousal in the Category Page
+                    tpj('#slides').slidesjs({
+                        width: 1020,
+                        height: 456,
+                        navigation: {
+                          active: false,
+                            // [boolean] Generates next and previous buttons.
+                            // You can set to false and use your own buttons.
+                            // User defined buttons must have the following:
+                            // previous button: class="slidesjs-previous slidesjs-navigation"
+                            // next button: class="slidesjs-next slidesjs-navigation"
+                          effect: "slide"
+                            // [string] Can be either "slide" or "fade".
+                        },
+                        play: {
+                          active: false,
+                            // [boolean] Generate the play and stop buttons.
+                            // You cannot use your own buttons. Sorry.
+                          effect: "slide",
+                            // [string] Can be either "slide" or "fade".
+                          interval: 5000,
+                            // [number] Time spent on each slide in milliseconds.
+                          auto: true,
+                            // [boolean] Start playing the slideshow on load.
+                          swap: true,
+                            // [boolean] show/hide stop and play buttons
+                          pauseOnHover: false,
+                            // [boolean] pause a playing slideshow on hover
+                          restartDelay: 2500
+                            // [number] restart delay on inactive slideshow
+                        }
+                      });
+
                 });
                 
                
